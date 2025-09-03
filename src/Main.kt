609e8c6f85af509dd580a8525ccda8aa6e94673c
Viktar Salauyou <night_elf@tut.by>
2025-09-03 09:17:54 +0300

    Test commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    fun Task_01() {
+        println("Viktar Salauyou Lesson3 Homework")
+        println("exercise_01 polindrom number")
 
+
+
+    }
+Task_01
 }
