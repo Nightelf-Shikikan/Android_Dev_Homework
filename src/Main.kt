3d9ebf9e3f644e91ee2b53c66ad3962e41ce7e52
Viktar Salauyou <night_elf@tut.by>
2025-09-10 19:46:35 +0300

    Homework05 ver 0.9 add branch for hm3

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,95 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun reverseOrderOfWords(originalSentence: String): String {
-    return originalSentence.split(" ") // Splits the string into a list of words using space as a delimiter
-        .reversed()           // Reverses the order of elements in the list
-        .joinToString(" ")    // Joins the list of words back into a single string, separated by spaces
-
-
-}
-
-
 fun main() {
-    println("Viktar Salauyou Lesson5 Homework")
-    println("exercise 01 receive words and restructure them backwards ")
+    fun Task_01() {
+        println("Viktar Salauyou Lesson3 Homework")
+        println("exercise 01 palindrome number")
+        println("Input a number to the console to check if it a palindrome number or not")
+        var number = readLine()!!.toInt()
+        val original = number
 
-    val originalSentence = "This is a sample sentence"
-    val reversedSentence = reverseOrderOfWords(originalSentence)
+        var reversed = 0
+        while (number > 0) {
+            val digit = number % 10       // take the last digit
+            reversed = reversed * 10 + digit // build reversed number
+            number /= 10                  // remove the last digit
+        }
+
+        if (original == reversed) {
+            println("$original is a palindrome")
+        } else {
+            println("$original is not a palindrome")
+        }
+    }
+
+    fun Task_02() {
+        println("exercise_02 Prime number")
+        println("Input a number to the console to check if it a Prime Number or not")
+        var number = readLine()!!.toInt()
+
+        if (number <= 1) {
+            println("$number is not a prime number")
+            return
+        }
+        var isPrime = true
+        for (i in 2 until number) {   // range from 2 to number-1
+            if (number % i == 0) {    // check if i divides number evenly
+                isPrime = false
+                break
+            }
+        }
+
+        if (isPrime) {
+            println("$number is a prime number")
+        } else {
+            println("$number is not a prime number")
+        }
+
+    }
+
+    fun Task_03() {
+        println("exercise_03 calculating a sum of digits in a number and repeating the process until result is a single digit number")
+        println("Input a number to the console to begin the calculation process")
+
+        var number = readLine()!!.toInt()
+
+        while (number >= 10) {   // keep going until it's a single digit
+            var sum = 0
+            var temp = number
 
 
+            // sum all digits
+            while (temp > 0) {
+                sum += temp % 10   // take last digit
+                temp /= 10         // remove last digit
+            }
+            number = sum   // update number with the sum
+        }
+
+        println("Final single digit result: $number")
+    }
 
 
+    fun Task_04() {
 
-    println("Original: $originalSentence")
-    println("Reversed: $reversedSentence") // Output: sentence sample a is This
+        println("exercise_01 inverted Ladder")
+        println("Input value for inverted ladder height")
+        val number = readLine()!!.toInt()
+
+        for (i in number downTo 1) {          // from height down to 1
+            for (j in 1..i) {                 // print i times '#'
+                print("#")
+            }
+            println()                         // move to next line
+        }
+
+
+    }
+
+
+    Task_01()
+    Task_02()
+    Task_03()
+    Task_04()
 
 }
