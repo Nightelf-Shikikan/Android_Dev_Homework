d066015a27fd3c3b53e11b06edd95ee5c9f2c62a
Viktar Salauyou <night_elf@tut.by>
2025-10-02 19:49:08 +0300

    Homework13 ver 0.9

--- src/Main.kt
+++ src/Main.kt
@@ -1,80 +1,71 @@
-import java.util.InputMismatchException
+import Node
+import LinkedList
 
-class TooYongException(message: String) : Exception(message)
+fun ReturnSum() {
+    println("Task 01 input numbers and calculate their sum")
 
-fun validateAge(age: Int) {
-    if (age <= 17) {
-        throw TooYongException("Must be 18 or older")
+    println("Please enter numbers in line using spase as divider")
+    val arrayofnumbers = readln()
+        .split(" ")
+        .map { it.toInt() }
+
+    val result = arrayofnumbers.sum()
+    println("Sum of all numbers is: $result")
+
+
+}
+
+fun PeopleList() {
+    class Person(val name: String, val age: Int, val sex: String)
+
+    val person01 = Person("Bob", 23, "male")
+    val person02 = Person("Jack", 18, "male")
+    val person03 = Person("Tom", 11, "male")
+    val person04 = Person("Samantha", 33, "female")
+    val person05 = Person("Lidia", 21, "female")
+    val person06 = Person("Monica", 12, "female")
+
+
+    val listOfPersons = listOf(
+        person01, person02, person03, person04, person05, person06
+
+    )
+    val under18 = listOfPersons.filter { it.age < 18 }
+
+    println("People under 18:")
+    for (p in under18) {
+        println("${p.name}, age ${p.age}")
+    }
+    val adults = listOfPersons.filter { it.age >= 18 }
+    // 🔹 Sort them by age (ascending)
+    val sortedAdults = adults.sortedBy { it.age }
+
+
+
+    println("People 18 and older, sorted by age:")
+    //   sortedAdults.let { list ->
+    //      list.forEach { println("${it.name} is ${it.age} years old") }
+    //  }
+    sortedAdults.apply {
+        forEach { println("${it.name} is ${it.age} years old") }
     }
 
 }
 
+fun CalculatingNumbers() {
+    val numbers = List(size = 1000001, { it.toLong() })
+    val filterNumbers = numbers.asSequence().filter { it % 2L == 0L }.map { it * it }.sum()
 
-fun Task01() {
-    println("Task 01 divide numbers and check for exeptions")
-
-    try {
-        println("input first number")
-        var num01 = readln().toInt()
-
-        println("input second number")
-        var num02 = readln().toInt()
-
-        val result: Int = num01 / num02
-        println("Result: $result")
-
-    } catch (a: ArithmeticException) {
-        println("Error: can't divide by zero")
-    } catch (b: InputMismatchException) {
-        println("Error: Invalod input")
-    }
-
-}
-
-fun Task02() {
-
-    println("Task 02 Checking the age of a person")
-
-    try {
-        println("Enter person's Age")
-        val age = readln().toInt()
-
-        validateAge(age)
-
-        println("Your age is: $age")
-
-    } catch (a: TooYongException) {
-        println("Error: Must be 18 or older")
-
-    } catch (e: NumberFormatException) {
-        println("Error: Please enter a valid number")
-    }
-}
-
-fun Task03() {
-
-    println("Task 03 divide numbers and check for exeptions")
-    try {
-        println("Enter a string:")
-        val input = readln()
-
-        if (input.isEmpty()) {
-            throw IllegalArgumentException("Input cannot be empty")
-        }
-
-        println("You entered: $input")
-
-    } catch (e: IllegalArgumentException) {
-        println("Error: ${e.message}")
-    }
+    println(filterNumbers)
 
 }
 
 
 fun main() {
-    Task01()
-    Task02()
-    Task03()
+    ReturnSum()
+    PeopleList()
+    CalculatingNumbers()
 }
+
 
 
